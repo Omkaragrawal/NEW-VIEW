068acc598f90ce454c30502f2a8975d1a7e3c956
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2020-07-31 19:13:15 +0530

    Add test file

---
+++ node.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+const { expect } = require('chai');
+// const server = require('../index');
+
+describe('test', () => {
+  it('should return a string', () => {
+    expect('ci with travis').to.equal('ci with travis');
+  });
+});
