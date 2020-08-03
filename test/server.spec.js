326e88f0323b3d3cec441925717abfe749d11671
Ebenezer <ebenezerebi500@gmail.com>
2020-08-03 15:23:38 +0530

    changed the structure of the project

--- test/server.spec.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('chai');
-// const server = require('../index');
-
-describe('test', () => {
-  it('should return a string', () => {
-    expect('ci with travis').to.equal('ci with travis');
-  });
-});
