e79eabb74705e1d688065197b7ab8c3ce8d8677f
Ebenezer <60901996+ebi10sa22@users.noreply.github.com>
2020-08-27 10:40:19 +0530

    updated Changes

--- front-end/src/Components/IntroPage/Signup.js
+++ front-end/src/Components/IntroPage/Signup.js
@@ -22,17 +22,17 @@ class Signup extends React.Component {
                 <h1>Create your Account</h1>
                 <div className="inputBox">
                     <p>First name</p>
-                    <input type="text"/>
+                    <input type="text" label="firstname"/>
                     <p>Last name</p>
-                    <input type="text"/>
+                    <input type="text" label="lastname"/>
                     <p>Email</p>
-                    <input type="text"/>
+                    <input type="text" label="Email"/>
                     <p>Mobile Number</p>
-                    <input type="text"/>
+                    <input type="text" label="mobilenumber"/>
                     <p>Password</p>
-                    <input type="text"/>
+                    <input type="text" label="password"/>
                     <p>Conform your Password</p>
-                    <input type="text"/>
+                    <input type="text" label="cpassword"/>
                 </div>
                 <div className="buttons">
                     <button type="button" onClick={this.handleOnClick}>SUBMIT</button>
