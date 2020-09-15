cd4101d36d83cd47927aecb1485424d02905309e
Ebenezer J <ebenezerebi500@gmail.com>
2020-09-16 01:13:48 +0530

    updated

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,30 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import './index.css'
-import Login from './Components/IntroPage/Login'
-import Signup from './Components/IntroPage/Signup'
-import Landing from './Components/IntroPage/Landing'
-import { BrowserRouter, Link ,Switch , Route} from 'react-router-dom';
-import Api from './Components/SearchPage/Api'
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
+import Login from "./Components/IntroPage/Login";
+import Signup from "./Components/IntroPage/Signup";
+import Landing from "./Components/IntroPage/Landing";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import Api from "./Components/SearchPage/Api";
 
 ReactDOM.render(
-//  <BrowserRouter>
-//   <div className="nav">
-//       <ul>
-//         <li><Link to ="/" style={{color:"white"}} >Home</Link></li>
-//         <li><Link to ="/login" style={{color:"white"}}>Login</Link></li>
-//         <li><Link to ="/signup" style={{color:"white"}}>Signup</Link></li>
-//       </ul>
-//       <Switch>
-//         <Route exact path="/" component={Landing}></Route>
-//         <Route exact path="/login" component={Login}></Route>
-//         <Route exact path="/signup" component={Signup}></Route>
-//       </Switch>
-//   </div>
-//   </BrowserRouter>
-<Api />  ,
-  document.getElementById('root')
+  <BrowserRouter>
+    <div className="nav">
+      <ul>
+        <li>
+          <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="/login" style={{ color: "white", textDecoration: "none" }}>
+            Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/signup" style={{ color: "white", textDecoration: "none" }}>
+            Signup
+          </Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={Landing}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/signup" component={Signup}></Route>
+      </Switch>
+    </div>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
