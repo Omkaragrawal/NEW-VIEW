991761d588b236a9104c395e2f06e0cc3d60ff52
Ebenezer J <ebenezerebi500@gmail.com>
2020-09-16 01:23:26 +0530

    Added router

---
+++ front-end/src/Components/LangingPage/Components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+function NavBar() {
+  return (
+    <div className="nav__bar">
+      <Link className="nav__link" to="/home">
+        <img
+          className="nav__logo"
+          src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_1-5bdc75aaebeb75dc7ae79426ddd9be3b2be1e342510f8202baf6bffa71d7f5c4.svg"
+          alt="logo"
+        />
+      </Link>
+      <Link className="nav__link" to="/login">
+        <div className="nav__login">
+          <p className="nav__loginLine">Sign In</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default NavBar;
