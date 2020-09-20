dfd92e325d3f672a19cb98bc6d2df9fe89aedfc2
Ebenezer J <ebenezerebi500@gmail.com>
2020-09-20 15:50:21 +0530

    Updated

---
+++ front-end/build/precache-manifest.e69cda13de606a183b1d4a0a7470c11e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ebd9b2f8a920ca200b281a70042b82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e184842c48fe2af9d6af",
+    "url": "/static/css/main.b5da8d0e.chunk.css"
+  },
+  {
+    "revision": "d97fa26d0a3b5a9bc22b",
+    "url": "/static/js/2.ae25d4f4.chunk.js"
+  },
+  {
+    "revision": "f3a6a51dd260c4f924bf796e7b33f546",
+    "url": "/static/js/2.ae25d4f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e184842c48fe2af9d6af",
+    "url": "/static/js/main.606b2bf1.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  },
+  {
+    "revision": "032aacbec05751d04dfd3fefdee6660d",
+    "url": "/static/media/tmdb.032aacbe.svg"
+  }
+]);
