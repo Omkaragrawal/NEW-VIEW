f2551f4d6b1794ab53574bf07f376f7a210503a8
Ebenezer J <ebenezerebi500@gmail.com>
2020-09-29 19:59:26 +0530

    Removed duplicate lines and accepted pull commit

--- server.js
+++ server.js
@@ -8,12 +8,7 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const compression = require("compression");
-<<<<<<< HEAD
-const { check, validationResult, sanitizeBody } = require("express-validator");
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const cors = require("cors");
-=======
 const session = require("express-session");
 const SessionStore = require("express-session-sequelize")(session.Store);
 const pg = require("pg");
@@ -30,16 +25,12 @@ var sequelize = require("./models/databases");
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
->>>>>>> 56654c47ff144715739413ed4b9fcf9ebbf68116
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-<<<<<<< HEAD
-app.use(cors());
-=======
 app.use(
   session({
     genid: function (req) {
@@ -61,7 +52,6 @@ app.use(
 require("./config/passport");
 app.use(passport.initialize());
 app.use(passport.session());
->>>>>>> 56654c47ff144715739413ed4b9fcf9ebbf68116
 app.use(compression());
 app.use(helmet());
 app.use(bodyParser.json());
@@ -71,12 +61,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-<<<<<<< HEAD
-=======
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
->>>>>>> 56654c47ff144715739413ed4b9fcf9ebbf68116
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
