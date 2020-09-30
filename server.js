0d36a940f152fe0eef29e25f763cbb348705648e
MOHIT BHAT <43911437+mbcse@users.noreply.github.com>
2020-10-01 00:33:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -60,6 +60,7 @@ app.use(logger("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
