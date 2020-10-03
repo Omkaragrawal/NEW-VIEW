de3929379ceab8fe7089055409901cd6fbc2c6d3
Ebenezer J <ebenezerebi500@gmail.com>
2020-10-03 16:15:18 +0530

    Ran pre-commit

---
+++ public/precache-manifest.e74adccd0a623bdc99f98c2dfd380b20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cea505d0fd3377360cbd476130e4d380",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3246cfd4c8da483b1868",
+    "url": "/static/css/main.82aed12f.chunk.css"
+  },
+  {
+    "revision": "bc4be89d1219cb432e95",
+    "url": "/static/js/2.ac5bdeb2.chunk.js"
+  },
+  {
+    "revision": "f3a6a51dd260c4f924bf796e7b33f546",
+    "url": "/static/js/2.ac5bdeb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3246cfd4c8da483b1868",
+    "url": "/static/js/main.ae1a6639.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9b04e15f7fd16014c14f5e11dd122244.js"
+  "/precache-manifest.e74adccd0a623bdc99f98c2dfd380b20.js"
 );
 
 self.addEventListener('message', (event) => {
